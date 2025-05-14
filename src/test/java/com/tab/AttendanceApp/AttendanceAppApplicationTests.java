aec10ff5e6da796f5c695dbe53684ebcd9b24723
MD TABREZ ALAM <tabrez@klizos.com>
2025-05-14 18:44:27 +0530

    initial commit

---
+++ src/test/java/com/tab/AttendanceApp/AttendanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tab.AttendanceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
